0caeaac05fbb17812477c22caf1855e61175213f
krkimchi <yoonseong1112@gmail.com>
2025-07-30 18:21:50 -0500

    feature/change-password

--- src/main/java/com/medicalsuppliesmanagement/service/IAuthService.java
+++ src/main/java/com/medicalsuppliesmanagement/service/IAuthService.java
@@ -1,9 +1,12 @@
 package com.medicalsuppliesmanagement.service;
 
 import com.medicalsuppliesmanagement.entity.UserAccount;
+import com.medicalsuppliesmanagement.dto.ChangePasswordDto;
 import java.util.Optional;
 
 public interface IAuthService {
     boolean authenticate(String username, String password);
     Optional<UserAccount> findByUsername(String username);
+    void save(UserAccount userAccount);
+    boolean changePassword(String username, ChangePasswordDto dto);
 } 
