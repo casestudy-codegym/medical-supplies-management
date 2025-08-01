02f1e258b4ae513322720e7522686d00d8d5d637
philau2512 <dplau25122002@gmail.com>
2025-08-01 20:20:42 +0700

    fix path

--- src/main/java/com/medicalsuppliesmanagement/controller/EmployeeController.java
+++ src/main/java/com/medicalsuppliesmanagement/controller/EmployeeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.security.Principal;
 
 @Controller
-@RequestMapping("/employee")
+@RequestMapping("/employees")
 public class EmployeeController {
 
     @Autowired
