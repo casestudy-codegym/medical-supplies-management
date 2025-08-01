c18883ed80b9282fc9e6581d38e29837d6f39501
nvrmq <155294370+nvrmq@users.noreply.github.com>
2025-08-01 16:33:42 +0700

    Revert "Feature/header footer"

--- src/main/java/com/medicalsuppliesmanagement/repository/ICategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.medicalsuppliesmanagement.repository;
-
-import com.medicalsuppliesmanagement.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoryRepository extends JpaRepository<Category, Long> {
-}
